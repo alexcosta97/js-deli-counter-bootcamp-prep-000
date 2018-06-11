dcdfb5f1f9f6088447fd12a65fdbf628c710d73d
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-11 15:19:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function takeANumber(katzDeliLine, name)
+{
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine[0] === undefined)
+  {
+    return `There is nobody waiting to be served!`;
+  }
+  else
+  {
+    name = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${name}.`;
+  }
+}
+
+function currenLine(katzDeliLine)
+{
+  if(katzDeliLine[0] === undefined)
+  {
+    return `The line is curently empty.`;
+  }
+  else
+  {
+    var string = '';
+    for(var i = 0; i < katzDeliLine.length; i++)
+    {
+      string += katzDeliLine[i];
+    }
+    return string;
+  }
+}
