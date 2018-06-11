75230edf4cf97774e0865168e4a05a52cca7aeea
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-11 15:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine)
 {
   if(katzDeliLine[0] === undefined)
   {
-    return `The line is curently empty.`;
+    return `The line is currently empty.`;
   }
   else
   {
